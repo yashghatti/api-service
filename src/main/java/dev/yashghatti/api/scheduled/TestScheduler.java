8312e7281e93945b2027b53c9a60d6b226aa080f
yghatti <yashghatti@gmail.com>
2022-09-07 13:52:47 -0600

    turned off scheduling

--- src/main/java/dev/yashghatti/api/scheduled/TestScheduler.java
+++ src/main/java/dev/yashghatti/api/scheduled/TestScheduler.java
@@ -3,7 +3,6 @@ package dev.yashghatti.api.scheduled;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 
 @Configuration
 @EnableScheduling
